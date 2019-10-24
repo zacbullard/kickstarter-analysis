f08157b676bef10d854d9a033ccd8c4f457a17c5
zacbullard <zacbullard@gmail.com>
2019-10-23 21:48:33 -0400

    Created boolean column for regex delivery confirmation.

--- tagging.py
+++ tagging.py
@@ -20,25 +20,20 @@ def mark_delivered_regex(cmdf):
     #This regex expression essentially looks for words that confirm delivery, without being preceeded by a negative.
     delivered_regex = re.compile("(?<!not\s)(?<!hasen't\s)(?<!hasn't\s)(?<!haven't\sI\s)(?<!haven't\s)(?<!ever\s)(?<!never\s)(?<!yet\s)(recieved|received|arrived)", re.IGNORECASE)
 
-    all_delivered_comments = []
-    all_delivered_dates = []
-    for i in range(len(cmdf.comments)):
-        delivered_comments = []
-        delivered_dates = []
-        if delivered_regex.findall(cmdf.comments.iloc[i]):
-
-    cmdf['delivered_comments'] = all_delivered_comments
-    cmdf['delivered_comment_count'] = cmdf.delivered_comments.apply(lambda x: len(x))
-    cmdf['delivered_dates'] = all_delivered_dates
+    cmdf['delivered_regex'] = cmdf.comments.map(lambda x: len(delivered_regex.findall(x))>0)
     
-
+        #Rearrange the dataframe to put the comments (long data) at the end.
+    cmdf = cmdf[['project_id','project_comment_count','reward_date','comment_date','delivered_regex','comments']]
     
-if __name__ = '__main__':
+ 
+if __name__ == '__main__':
     start_time = time.time()    
     print("Starting program...")
     
-    ksdf = pd.read_pickle(params.cleaned_kickstarter_pickle)
     cmdf = pd.read_pickle(params.cleaned_comments_pickle)    
     
+    mark_delivered_regex(cmdf)
+    
+    cmdf.to_pickle(params.cleaned_comments_pickle)
     
     print("Finished! In", (time.time()-start_time)/60," minutes")
